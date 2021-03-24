e576c8974ca67abf85fc6402d0303963218be93e
Sherlock Wisdom <wisdomnji@gmail.com>
2021-03-24 20:51:16 +0100

    update: platforms extract more content successfully

--- src/platforms.py
+++ src/platforms.py
@@ -7,30 +7,41 @@ class Platforms:
 
     # TODO: this are created in a dir called platforms and would be parsed to get them
     def __init__(self, platform):
-        self.DEFAULT_PROVIDERS_PATH = "platforms/"
+        self.DEFAULT_PROVIDERS_PATH = "Platforms"
         # scan for all submodules
         providers = {}
         for root, dirs, files in os.walk( self.DEFAULT_PROVIDERS_PATH ):
             for _dir in dirs:
                 providers[_dir] = []
-                for root, dirs, files in os.walk( self.DEFAULT_PROVIDERS_PATH + _dir ):
+                for root, dirs, files in os.walk( self.DEFAULT_PROVIDERS_PATH+"/"+_dir ):
+                    print(f"Files: {files}")
                     for _file in files:
-                        if _file.split('.')[0] == _dir:
-                            providers[_dir].append( _file )
+                        if _file.split('_')[0] == _dir:
+                            splitted_filename = _file.split('.')[:-1]
+                            merged_filename = '_'.join(splitted_filename)
+                            print("MF:>>", merged_filename)
+                            providers[_dir].append(merged_filename)
+                            break
                     break
-                
             break
         print( providers )
 
         for provider in providers:
-            if not platform in providers[provider]:
-                raise Exception(f"Unknown platform: {platform}")
+            def_platform = f"{provider}_{platform}"
+            print("def_platform:", def_platform)
+
+            if not def_platform in providers[provider]:
+                raise Exception("Unknown platform:", def_platform)
             else:
                 self.platform_name = platform
 
                 # Being platform abstractions here
                 importlib.invalidate_caches()
-                self.platform = importlib.import_module(platform)
+                
+                LIB_NAME = f"{self.DEFAULT_PROVIDERS_PATH}.{provider}"
+                LIB = f".{def_platform}"
+                print(f"({LIB},{LIB_NAME})")
+                self.platform = importlib.import_module(LIB, LIB_NAME)
 
     def execute(self, protocol, body, userDetails):
         try:
