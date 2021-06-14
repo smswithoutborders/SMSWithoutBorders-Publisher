51ae7d69d6eb1cec232f0bca4a7868ad75f3c7c8
Sherlock Wisdom <wisdomnji@gmail.com>
2021-06-14 12:06:13 +0000

    merged twilio with current router

--- main.py
+++ main.py
@@ -178,101 +178,127 @@ def sync(session_id):
         print(traceback.format_exc())
         return jsonify({"status":500, "message":"internal error"})
 
+
+@app.route('/twilio_messages', methods=['POST', 'GET'])
+def incoming_messages():
+    From=request.values.get('From', None)
+    To=request.values.get('To', None)
+    FromCountry=request.values.get('FromCountry', None)
+    NumSegments=request.values.get('NumSegments', None)
+    Body=request.values.get('Body',None)
+
+    # print(f"From: {From}\nTo: {To}\nBody: {Body}\nFromCountry: {FromCountry}\nNumSegments: {NumSegments}")
+    '''
+    try:
+        router_url = CONFIGS["CLOUD_API"]["url"]
+        print(f"[+] Router url: {router_url}")
+        api_request=requests.post(f"{router_url}/messages", json={"text":Body, "phonenumber":From})
+    except Exception as error:
+        print( error )
+    else:
+        print( api_request.text )
+    
+    return api_request.text
+    '''
+    forward = {}
+    forward["phonenumber"} = From
+    forward["text"} = Body
+    forward["From"] = "Twilio"
+
+    new_messages(forward)
+    
+    return ""
+
 @app.route('/messages', methods=['POST', 'GET'])
-def new_messages():
+def new_messages(forwarded=None):
+    request_body = None
     if request.method == 'POST':
         request_body = request.json
-        if request_body is None:
-            return jsonify({"status":401, "message":"invalid request, missing body"})
-        if not 'text' in request_body:
-            return jsonify({"status":400, "message":"missing text"})
+    elif forwarded is not None:
+        request_body = forwarded
 
-        if not 'phonenumber' in request_body:
-            return jsonify({"status":400, "message":"missing phonenumber"})
+    if request_body is None:
+        return jsonify({"status":401, "message":"invalid request, missing body"})
+    if not 'text' in request_body:
+        return jsonify({"status":400, "message":"missing text"})
 
-        text = request_body["text"]
-        phonenumber = request_body["phonenumber"]
-        phonenumber = isp.rm_country_code(phonenumber)
-        timestamp=""
-        discharge_timestamp=""
-        if "timestamp" in request_body:
-            timestamp = request_body["timestamp"]
-        if "discharge_timestamp" in request_body:
-            discharge_timestamp = request_body["discharge_timestamp"]
+    if not 'phonenumber' in request_body:
+        return jsonify({"status":400, "message":"missing phonenumber"})
 
-        print(f"{phonenumber}|{text}")
+    text = request_body["text"]
+    phonenumber = request_body["phonenumber"]
+    phonenumber = isp.rm_country_code(phonenumber)
+    timestamp=""
+    discharge_timestamp=""
+    if "timestamp" in request_body:
+        timestamp = request_body["timestamp"]
+    if "discharge_timestamp" in request_body:
+        discharge_timestamp = request_body["discharge_timestamp"]
 
-        return_json = {"status" :"", "body":""}
-        try: 
-            user_details = acquire_requester(phonenumber)
-            # authenticate request
-            if len(user_details) < 1:
-                return jsonify({"status":401, "message":"requester not synced"})
+    print(f"{phonenumber}|{text}")
 
-            print("[+] - User authenticated")
-            messageID=None
+    return_json = {"status" :"", "body":""}
+    try: 
+        user_details = acquire_requester(phonenumber)
+        # authenticate request
+        if len(user_details) < 1:
+            return jsonify({"status":401, "message":"requester not synced"})
 
-            # parse the contents of the SMS message
-            # print(user_details)
-            h_password = cloudfunctions.cloudAcquireGrantLevelHashes(user_details[0]['user_id'])
-            if not 'password_hash' in h_password:
-                return jsonify({"status":403, "message":"failed to get required hashes"})
+        print("[+] - User authenticated")
+        messageID=None
 
-            user_details[0]["password_hash"] = h_password
-            parsedText = routerfunctions.routerParseText(text, user_details[0])
+        # parse the contents of the SMS message
+        # print(user_details)
+        h_password = cloudfunctions.cloudAcquireGrantLevelHashes(user_details[0]['user_id'])
+        if not 'password_hash' in h_password:
+            return jsonify({"status":403, "message":"failed to get required hashes"})
 
-            # check for a valid protocol being returned
-            if parsedText is not None and "protocol" in parsedText:
+        user_details[0]["password_hash"] = h_password
+        parsedText = routerfunctions.routerParseText(text, user_details[0])
 
-                # Authenticate acquire stored stoken information for users
-                userDetails = cloudfunctions.cloudAuthUser(user_id=user_details[0]['user_id'], phonenumber=phonenumber, protocol=parsedText["protocol"], platform=parsedText["platform"], provider=parsedText["provider"])
-                # userDetails = cloudfunctions.cloudAuthUser("gmail", "send", phonenumber)
+        # check for a valid protocol being returned
+        if parsedText is not None and "protocol" in parsedText:
 
-                if userDetails is not None:
-                    if len(userDetails) > 0:
-                        try:
-                            # platform = Platforms("gmail")
-                            platform = Platforms(platform=parsedText["platform"])
-                            results = platform.execute(parsedText["protocol"], parsedText["body"], userDetails)
-                        except Exception as error:
-                            raise Exception(error)
-                        else:
-                            if results:
-                                print(f"[+] Successfully executed for platform - {results}")
-                                return_json["status"] = 200
-                                return_json["body"] = f"successfully executed for platform - {results}"
-                            else:
-                                print(f"[+] Failed to execute for platform - {results}")
-                                return_json["status"] = 500
-                                return_json["body"] = results
+            # Authenticate acquire stored stoken information for users
+            userDetails = cloudfunctions.cloudAuthUser(user_id=user_details[0]['user_id'], phonenumber=phonenumber, protocol=parsedText["protocol"], platform=parsedText["platform"], provider=parsedText["provider"])
+            # userDetails = cloudfunctions.cloudAuthUser("gmail", "send", phonenumber)
+
+            if userDetails is not None:
+                if len(userDetails) > 0:
+                    try:
+                        # platform = Platforms("gmail")
+                        platform = Platforms(platform=parsedText["platform"])
+                        results = platform.execute(parsedText["protocol"], parsedText["body"], userDetails)
+                    except Exception as error:
+                        raise Exception(error)
                     else:
-                        return_json["status"] = 404
-                        raise Exception(f"no token stored in wallet")
+                        if results:
+                            print(f"[+] Successfully executed for platform - {results}")
+                            return_json["status"] = 200
+                            return_json["body"] = f"successfully executed for platform - {results}"
+                        else:
+                            print(f"[+] Failed to execute for platform - {results}")
+                            return_json["status"] = 500
+                            return_json["body"] = results
                 else:
-                    return_json["status"] = 403
-                    print(userDetails)
-                    raise Exception(f"Failed to authenticate user/request...")
+                    return_json["status"] = 404
+                    raise Exception(f"no token stored in wallet")
             else:
-                return_json["status"] = 400
-                raise Exception(f"Could not determine protocol in parsedText: {parsedText}")
-                    
-        except Exception as err:
-            # return_json["status"] = request.status_code
-            # return_json["status"] = 500
-            # return_json["body"] = request
-            print(err)
-            return_json["body"] = str(err)
+                return_json["status"] = 403
+                print(userDetails)
+                raise Exception(f"Failed to authenticate user/request...")
+        else:
+            return_json["status"] = 400
+            raise Exception(f"Could not determine protocol in parsedText: {parsedText}")
+                
+    except Exception as err:
+        # return_json["status"] = request.status_code
+        # return_json["status"] = 500
+        # return_json["body"] = request
+        print(err)
+        return_json["body"] = str(err)
     
     return jsonify(return_json)
-
-def print_ip():
-    import socket
-    h_name = socket.gethostname()
-    IP_addres = socket.gethostbyname(h_name)
-    print("Host Name is:" + h_name)
-    print("Computer IP Address is:" + IP_addres)
-
-
 
 if CONFIGS["API"]["DEBUG"] == "1":
     # Allows server reload once code changes
