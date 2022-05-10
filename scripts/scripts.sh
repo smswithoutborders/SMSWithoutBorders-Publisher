6fb3b10baf80e879b1acd08a329a8de18e193924
s. wisdom <wisdomnji@gmail.com>
2022-05-10 21:45:41 +0000

    update: rethinking the entire publisher module

--- scripts/scripts.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-# opens port 6969 to all incoming tcp traffic
-sudo ufw allow from any to any port 6969 proto tcp
-
-# deny access - NOT TESTED
-sudo ufw deny from any to any port 6969 proto tcp
--- test.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.insert(0, '/var/www/Deku-Router')
-
-from main import app as application
-
-application.run()
