c6fc54c5ac9cf5ef8503fd507bef8bfe76b53ff4
s. wisdom <wisdomnji@gmail.com>
2022-05-14 22:51:57 +0000

    update: template for relative import in place, now to fix external lib methods

--- platforms/main.py
+++ platforms/main.py
@@ -2,6 +2,7 @@
 
 import logging
 import importlib
+import importlib.util
 import os
 
 logging.basicConfig(level='DEBUG')
@@ -11,14 +12,16 @@ class Platforms:
         platforms_path = os.path.join(os.path.dirname(__file__), 'available')
         platforms = {}
 
-        for root, available_dirs, files in os.walk( platforms_path ):
+        for available_root, available_dirs, files in os.walk( platforms_path ):
             for available_dir in available_dirs:
 
 
                 """
                 Begins scanning independent platforms
                 """
-                for root, dirs, available_platform_files in os.walk( platforms_path + "/" + available_dir ):
+                for root, dirs, available_platform_files in os.walk( 
+                        platforms_path + "/" + available_dir ):
+
                     for available_file in available_platform_files:
 
                         # logging.debug("available files: %s", available_file)
@@ -31,35 +34,48 @@ class Platforms:
                             if len(platforms) < 1:
                                 platforms[available_dir] = []
 
-                            logging.debug("available_file: %s", filename_no_extension)
+                            platforms_path = os.path.join(
+                                    os.path.dirname(__file__), 
+                                    f'available/{filename_no_extension}', available_file)
 
-                            platforms[available_dir].append(filename_no_extension)
+                            platforms[available_dir] = platforms_path
 
                             break
         return platforms
 
 
-    def import_available_platform(self, platform: str) -> None:
+    def import_available_platforms(self, platforms: dict) -> None:
         """
-        for provider in providers:
-            def_platform = f"{provider}_{platform}"
-            print("def_platform:", def_platform)
-
-            if not def_platform in providers[provider]:
-                # raise Exception("Unknown platform:", def_platform)
-                continue
-            else:
-                platform_name = platform
-                provider = provider
-
-                # Being platform abstractions here
-                importlib.invalidate_caches()
-                
-                LIB_NAME = f"{platforms_path}.{provider}".split('/')[-1:][0]
-                LIB = f".{def_platform}"
-                print(f"({LIB},{LIB_NAME})")
-                platform = importlib.import_module(LIB, LIB_NAME)
         """
+
+        for platform_name, platform_filepath in platforms.items():
+
+            """
+            for provider in providers:
+                def_platform = f"{provider}_{platform}"
+                print("def_platform:", def_platform)
+
+                if not def_platform in providers[provider]:
+                    # raise Exception("Unknown platform:", def_platform)
+                    continue
+                else:
+                    platform_name = platform
+                    provider = provider
+
+                    # Being platform abstractions here
+                    importlib.invalidate_caches()
+                    
+                    LIB_NAME = f"{platforms_path}.{provider}".split('/')[-1:][0]
+                    LIB = f".{def_platform}"
+                    print(f"({LIB},{LIB_NAME})")
+                    platform = importlib.import_module(LIB, LIB_NAME)
+            """
+            spec = importlib.util.spec_from_file_location(platform_name, platform_filepath)
+            platform_module = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(platform_module)
+
+            dir(platform_module)
+
 
     def execute(self, protocol, body, userDetails):
         try:
