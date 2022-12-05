7d12860e1cd7e97ff4419d0f802f304e505ae72b
sherlock <wisdomnji@gmail.com>
2022-12-05 01:00:39 +0100

    update: retry methods included

--- main.py
+++ main.py
@@ -1,31 +1,50 @@
 #!/usr/bin/env python3
 
 import pika
+import random
+from retry import retry
 
-credentials=pika.PlainCredentials("sherlock", "asshole")
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-            host='staging.smswithoutborders.com',
-            credentials=credentials))
+@retry(pika.exceptions.AMQPConnectionError, delay=5, jitter=(1, 3))
+def consumer():
+    """
+    """
+    credentials=pika.PlainCredentials("sherlock", "asshole")
+    connection = pika.BlockingConnection(pika.ConnectionParameters(
+        heartbeat=600,
+        blocked_connection_timeout=300,
+        host='staging.smswithoutborders.com',
+        credentials=credentials))
 
-channel = connection.channel()
+    channel = connection.channel()
 
-result = channel.queue_declare(queue='unofficial-smswithoutborders-queue', 
-                               durable=True)
+    result = channel.queue_declare(queue='unofficial-smswithoutborders-queue', 
+                                   durable=True)
 
-queue_name = result.method.queue
+    queue_name = result.method.queue
 
-channel.queue_bind(exchange='smswithoutborders-exchange', 
-                   queue=queue_name, 
-                   routing_key="smswithoutborders-default-routing-key")
+    channel.queue_bind(exchange='smswithoutborders-exchange', 
+                       queue=queue_name, 
+                       routing_key="smswithoutborders-default-routing-key")
 
-print(' [*] Waiting for logs. To exit press CTRL+C')
+    print(' [*] Waiting for logs. To exit press CTRL+C')
 
-def callback(ch, method, properties, body):
-    print(" [x] %r" % body)
+    def callback(ch, method, properties, body):
+        print(" [x] %r" % body)
 
-channel.basic_consume(
-        queue=queue_name, 
-        on_message_callback=callback, 
-        auto_ack=True)
+    channel.basic_consume(
+            queue=queue_name, 
+            on_message_callback=callback, 
+            auto_ack=True)
 
-channel.start_consuming()
+    try:
+        channel.start_consuming()
+    except KeyboardInterrupt:
+        channel.stop_consuming()
+        connection.close()
+    except pika.exceptions.ConnectionClosedByBroker:
+        print("clean stop")
+        pass
+        # continue
+
+if __name__ == "__main__":
+    consumer()
